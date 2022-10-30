343d8db6b3eada2b24a8cfeac324854bc103800b
Nikhil Thadeshwar <NIKHILTHADESHWAR2@GMAIL.COM>
2022-10-30 15:27:44 +0530

    added controller

---
+++ src/main/java/com/webhook/Controller.java
@@ -0,0 +1,25 @@
+package com.webhook;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+  private final Logger LOGGER = LoggerFactory.getLogger(Controller.class);
+
+  @PostMapping("/delivery-report")
+  public void postReport(HttpServletRequest request) throws IOException {
+    LOGGER.debug("Endpoint is hit");
+    request.getReader().lines().forEach(s -> LOGGER.debug("Reader value : {}", s));
+  }
+}
