ea55d7045ba4b4354c6dd17ab73f7724fc88e3c4
Nikhil Thadeshwar <NIKHILTHADESHWAR2@GMAIL.COM>
2022-10-30 15:27:10 +0530

    initial commit

---
+++ src/main/java/com/webhook/WebhookWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.webhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhookWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebhookWorkflowApplication.class, args);
+	}
+
+}
